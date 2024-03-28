3fb56bf45a32b0e779db0b3ad958ae710ed76cd8
Olav Rorvik <olavr.dev@gmail.com>
2024-03-28 12:38:24 +0100

    added gameField logic

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,14 @@
 let editedPlayer = 0;
+let activePlayer = 0;
 
 const players = [
   {
     name: '',
-    symbol: 'X',
+    symbol: '<i class="fa-solid fa-x"></i>',
   },
   {
     name: '',
-    symbol: 'O',
+    symbol: '<i class="fa-solid fa-o"></i>',
   },
 ];
 
@@ -15,21 +16,24 @@ const playerConfigOverlayElement = document.getElementById('config-overlay');
 const backdropElement = document.getElementById('backdrop');
 const formElement = document.querySelector('form');
 const errorsOutputElement = document.getElementById('config-errors');
+const gameAreaElement = document.getElementById('active-game');
+const activePlayerNameElement = document.getElementById('active-player-name');
 
 const editPlayerOneBtnElement = document.getElementById('edit-player-1-btn');
 const editPlayerTwoBtnElement = document.getElementById('edit-player-2-btn');
 const cancelConfigBtnElement = document.getElementById('cancel-config-button');
 const startNewGameBtnElement = document.getElementById('start-new-game-btn');
 const startNewGameErrorElement = document.getElementById('new-game-error');
-
-const gameAreaElement = document.getElementById('active-game');
+const gameFieldElements = document.querySelectorAll('#game-board li');
 
 editPlayerOneBtnElement.addEventListener('click', openPlayerConfig);
 editPlayerTwoBtnElement.addEventListener('click', openPlayerConfig);
-
 cancelConfigBtnElement.addEventListener('click', closePlayerConfig);
 backdropElement.addEventListener('click', closePlayerConfig);
-
 formElement.addEventListener('submit', savePlayerConfig);
 
 startNewGameBtnElement.addEventListener('click', startNewGame);
+
+for (const gameFieldElement of gameFieldElements) {
+  gameFieldElement.addEventListener('click', selectGameField);
+}
--- scripts/game.js
+++ scripts/game.js
@@ -3,5 +3,21 @@ function startNewGame() {
     startNewGameErrorElement.style.display = 'block';
     return;
   }
+  activePlayerNameElement.textContent = players[activePlayer].name;
   gameAreaElement.style.display = 'block';
+}
+
+function switchPlayer() {
+  if (activePlayer === 0) {
+    activePlayer = 1;
+  } else {
+    activePlayer = 0;
+  }
+  activePlayerNameElement.textContent = players[activePlayer].name;
+}
+
+function selectGameField(event) {
+  event.target.innerHTML = players[activePlayer].symbol;
+  event.target.classList.add('disabled');
+  switchPlayer();
 }
